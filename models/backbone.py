fe23a1aded44f83257bf1a4d673c3129c072b6e3
kaiyuantee <kaiyuan.tee1@gmail.com>
2020-07-14 22:40:48 +0800

    FUCK ME

--- models/backbone.py
+++ models/backbone.py
@@ -88,7 +88,7 @@ class Backbone(BackboneBase):
                  dilation: bool):
         backbone = getattr(torchvision.models, name)(
             replace_stride_with_dilation=[False, False, dilation],
-            pretrained=False, norm_layer=FrozenBatchNorm2d)
+            pretrained=None, norm_layer=FrozenBatchNorm2d)
         num_channels = 512 if name in ('resnet18', 'resnet34') else 2048
         super().__init__(backbone, train_backbone, num_channels, return_interm_layers)
 
